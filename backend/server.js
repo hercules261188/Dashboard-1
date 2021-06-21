1cb9c1139ae25c1d7672b7b633230a94e0cc1817
AbhinavXT <abhinavpathaka17@gmail.com>
2021-06-21 21:01:50 +0530

    Backend route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 PORT = 5000
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`)
