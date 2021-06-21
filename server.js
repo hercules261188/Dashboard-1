95d7537757eeb9395fda91ef2f9f3d373309dd54
AbhinavXT <abhinavpathaka17@gmail.com>
2021-06-21 21:31:39 +0530

    Changed server dir

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require('path')
 const app = express()
 PORT = 5000
 
-app.use(express.static(path.join(__dirname, 'build')))
+console.log(__dirname)
+
+app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'))
+	res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
 })
 
 app.listen(PORT, () => {
