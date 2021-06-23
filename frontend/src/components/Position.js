aa56142a51c2110e334e9d216a4121891ce66dcd
AbhinavXT <abhinavpathaka17@gmail.com>
2021-06-23 22:32:46 +0530

    Added Dashboard Image

--- frontend/src/components/Position.js
+++ frontend/src/components/Position.js
@@ -1,25 +1,4 @@
-import { Doughnut } from 'react-chartjs-2'
-
 const Position = () => {
-	//const data = {
-	//	datasets: [
-	//		{
-	//			data: [45, 30, 25],
-	//			backgroundColor: [
-	//				'rgba(151,113,237,0.4)',
-	//				'rgba(99,102,241,0.4)',
-	//				'rgba(239,68,68,0.4)',
-	//			],
-	//			borderColor: [
-	//				'rgba(151,113,237)',
-	//				'rgba(99,102,241)',
-	//				'rgba(239,68,68)',
-	//			],
-	//			borderWidth: 2,
-	//		},
-	//	],
-	//}
-
 	return (
 		<div className='flex flex-col w-full h-full p-4 border border-gray-700 rounded-md'>
 			<div className='flex justify-between'>
