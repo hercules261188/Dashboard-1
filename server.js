4f5687c79645b701e548031c5cc1991805412bca
AbhinavXT <abhinavpathaka17@gmail.com>
2021-06-24 00:18:15 +0530

    Port

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+const dotenv = require('dotenv')
 const express = require('express')
 const path = require('path')
 const app = express()
-PORT = 5000
+
+dotenv.config()
 
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'))
 })
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`)
